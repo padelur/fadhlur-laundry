4ac8b1008cecd5274ce436c9cce9aec2bdb18d9a
padelur <fadhlurrahman1909@gmail.com>
2025-06-30 16:58:47 +0700

    template & homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.template')
+
+@section('title', 'Beranda')
+
+@section('content')
+<div class="text-center py-5">
+    <h1 class="mb-3">Selamat Datang di Fadhlur Laundry</h1>
+    <p class="lead">Kami siap membantu cucianmu jadi bersih dan wangi dalam waktu cepat.</p>
+
+</div>
+@endsection
